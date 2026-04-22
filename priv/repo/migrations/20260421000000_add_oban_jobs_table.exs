b18404300605fc127a957ddecbb446ca88396bfa
Nayan Jain <jain.nayan90@gmail.com>
2026-04-22 11:33:39 +0530

    fix(migration): update Oban jobs table migration version to 14

--- priv/repo/migrations/20260421000000_add_oban_jobs_table.exs
+++ priv/repo/migrations/20260421000000_add_oban_jobs_table.exs
@@ -2,10 +2,10 @@ defmodule AdButler.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
   def up do
-    Oban.Migrations.up(version: 1)
+    Oban.Migrations.up(version: 14)
   end
 
   def down do
-    Oban.Migrations.down(version: 1)
+    Oban.Migrations.down(version: 14)
   end
 end
